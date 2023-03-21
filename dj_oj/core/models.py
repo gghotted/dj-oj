853dc2b524c0b08f7ce9593d6bfb92b99d3488c3
gghotted <gghotted2@naver.com>
2023-03-21 12:46:58 +0900

    Feat: BaseModel 추가

--- dj_oj/core/models.py
+++ dj_oj/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(
+        verbose_name='생성일',
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        verbose_name='수정일',
+        auto_now=True,
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
