454e2a225046ec4b85f980cacd1ae5dfbb10df3f
gghotted <gghotted2@naver.com>
2023-04-05 14:12:53 +0900

    Feat: 비밀번호 변경 추가

--- dj_oj/users/urls.py
+++ dj_oj/users/urls.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from users.views import LoginView, SignupView
+from users.views import LoginView, PasswordChangeView, SignupView
 
 app_name = 'users'
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
+    path('change-password/', PasswordChangeView.as_view(), name='change_password'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- dj_oj/users/views.py
+++ dj_oj/users/views.py
@@ -1,10 +1,12 @@
+from braces.views import LoginRequiredMixin
 from django.conf import settings
 from django.contrib.auth import login
 from django.contrib.auth.views import RedirectURLMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, FormView
 
-from users.forms import LoginForm, UserCreationForm
+from users.forms import (LoginForm, PasswordChangeForm, PasswordCheckForm,
+                         UserCreationForm)
 
 
 class SignupView(CreateView):
@@ -20,4 +22,33 @@ class LoginView(RedirectURLMixin, FormView):
 
     def form_valid(self, form):
         login(self.request, form.user)
+        return super().form_valid(form)
+
+
+class PasswordCheckView(LoginRequiredMixin, FormView):
+    template_name = 'users/check_password.html'
+    form_class = PasswordCheckForm
+    success_url = reverse_lazy('users:change_password')
+
+    def get_form_kwargs(self):
+        ctx = super().get_form_kwargs()
+        ctx['instance'] = self.request.user
+        return ctx
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+
+class PasswordChangeView(LoginRequiredMixin, FormView):
+    template_name = 'users/change_password.html'
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('users:login')
+
+    def get_form_kwargs(self):
+        ctx = super().get_form_kwargs()
+        ctx['instance'] = self.request.user
+        return ctx
+
+    def form_valid(self, form):
+        form.save()
         return super().form_valid(form)
