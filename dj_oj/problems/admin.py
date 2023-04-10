91733b690abfb9c04facc262b713aa61e830ac88
gghotted <gghotted2@naver.com>
2023-04-10 21:10:07 +0900

    Feat: image 항상 업데이트

--- dj_oj/problems/admin.py
+++ dj_oj/problems/admin.py
@@ -17,14 +17,12 @@ class DockerImageAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         obj.save()
-        if not change:
-
-            with obj.zip_file.unzip() as path:
-                client = docker.from_env()
-                client.images.build(
-                    path=str(path),
-                    tag=obj.tag,
-                )
+        with obj.zip_file.unzip() as path:
+            client = docker.from_env()
+            client.images.build(
+                path=str(path),
+                tag=obj.tag,
+            )
 
 
 @admin.register(Category)
